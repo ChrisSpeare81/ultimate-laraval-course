a4184318c72489d63ef7614189c4b6af22e0768c
Chris Speare <cspeare81@googlemail.com>
2024-08-13 13:00:44 +0000

    Lesson 106 - Tailwind form plugin and TextInput

--- job-board/resources/views/job/show.blade.php
+++ job-board/resources/views/job/show.blade.php
@@ -1,7 +1,4 @@
 <x-layout>
-    <x-breadcrumbs class='mb-4' :links="[
-        'Jobs' => route('jobs.index'),
-        $job->title => '#'
-    ]"/>
+    <x-breadcrumbs class='mb-4' :links="['Jobs' => route('jobs.index'),$job->title => '#']"/>
     <x-job-card :$job/>
 </x-layout>
