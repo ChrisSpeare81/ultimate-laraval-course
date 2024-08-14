6b98cbe8d5d0da7a89d0769941d0c7c1f4d27182
Chris Speare <cspeare81@googlemail.com>
2024-08-14 09:24:39 +0000

    Lesson 116 - Empoyer MMR

--- job-board/app/Models/Job.php
+++ job-board/app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model {
     use HasFactory;
@@ -12,6 +13,10 @@ class Job extends Model {
     public static array $experience = ['entry', 'intermediate', 'senior'];
 
     public static array $category = ['IT', 'Finance', 'Sales', 'Marketing'];
+
+    public function employer(): BelongsTo {
+        return $this->belongsTo(Employer::class);
+    }
 
     public function scopeFilter(Builder $query, array $filters): Builder {
         return $query->when($filters['search'] ?? null, function ($query, $search) {
