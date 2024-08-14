ebf3dda3d31cd35a965da14b70d13b5c3f368854
Chris Speare <cspeare81@googlemail.com>
2024-08-14 09:54:36 +0000

    Lesson 119 - Other Employer Jobs on card

--- job-board/app/Http/Controllers/JobController.php
+++ job-board/app/Http/Controllers/JobController.php
@@ -39,7 +39,10 @@ class JobController extends Controller {
      * Display the specified resource.
      */
     public function show(Job $job) {
-        return view('job.show', ['job' => $job->load('employer')]);
+        return view(
+            'job.show',
+            ['job' => $job->load('employer.jobs')]
+        );
     }
 
     /**
