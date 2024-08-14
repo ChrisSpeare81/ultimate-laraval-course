a96389e83ec1572617c7541ccbe6e105ce317c92
Chris Speare <cspeare81@googlemail.com>
2024-08-14 08:47:45 +0000

    Lesson 113 - Gradiant background, Filter experience, style filter button

--- job-board/resources/views/job/index.blade.php
+++ job-board/resources/views/job/index.blade.php
@@ -30,7 +30,7 @@
                 </div>
             </div>
 
-            <button class="w-full">Filter</button>
+            <x-button class="w-full">Filter</x-button>
         </form>
     </x-card>
 
