de11a33f2b09ea543b7158b8d69e322cde85836d
Chris Speare <cspeare81@googlemail.com>
2024-08-14 18:32:50 +0000

    Lesson 135 - Label Component

---
+++ job-board/resources/views/components/label.blade.php
@@ -0,0 +1,5 @@
+<label for="expected_salary" class="mb-2 block text-sm font-medium text-slate-900" for="{{ $for }}">
+    {{ $slot }} @if ($required)
+        <span>*</span>
+    @endif
+</label>
--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\MyJobApplicationsController;
-use App\Models\JobApplication;
 use Illuminate\Support\Facades\Route;
 
 
@@ -12,7 +11,7 @@ Route::get('', fn() => to_route('jobs.index'));
 
 Route::get('login', fn() => to_route('auth.create'))->name('login');
 Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
-Route::delete('auth', [AuthController::class], 'destory')->name('auth.destroy');
+Route::delete('auth', [AuthController::class, 'destroy'])->name('auth.destroy');
 
 Route::resource('auth', AuthController::class)->only(['create', 'store']);
 Route::resource('jobs', JobController::class)->only(['index', 'show']);
