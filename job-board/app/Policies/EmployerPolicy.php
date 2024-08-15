f1be3fe08046a3e0feeae843ff1079ad73f8cc39
Chris Speare <cspeare81@googlemail.com>
2024-08-15 06:58:46 +0000

    Lesson 138 - Employer Middleware

--- job-board/app/Policies/EmployerPolicy.php
+++ job-board/app/Policies/EmployerPolicy.php
@@ -7,19 +7,6 @@ use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
 class EmployerPolicy {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Employer $employer): bool {
-        return false;
-    }
 
     /**
      * Determine whether the user can create models.
@@ -33,26 +20,5 @@ class EmployerPolicy {
      */
     public function update(User $user, Employer $employer): bool {
         return $employer->user_id === $user->id;
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Employer $employer): bool {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Employer $employer): bool {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Employer $employer): bool {
-        return false;
     }
 }
--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\MyJobApplicationsController;
+use App\Http\Controllers\MyJobController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -22,4 +23,5 @@ Route::middleware('auth')->group(function () {
     Route::resource('my-job-applications', MyJobApplicationsController::class)->only(['index', 'destroy']);
 
     Route::resource('employer', EmployerController::class)->only(['create', 'store']);
+    Route::middleware('employer')->resource('my-jobs', MyJobController::class);
 });
